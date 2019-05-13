f40f52dd6716d95823fbc0896b00338bc2f8a79f
Alex Ssanya <alexssanya@Alexs-MacBook-Pro.local>
2019-05-13 18:03:50 +0300

    Add tests for user signup api [#165967586]

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.urls import reverse
 # Create your tests here.
+
+class TestUserSignUpApi(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = {
+            'fullname':'Alex Ssanya',
+            'email': 'alex@gmail.com',
+            'password': 'Password'
+        }
+    
+    def test_signup_api_exists(self):
+        url = reverse('users:signup_users')
+        response = self.client.get(url, self.user)
+        self.assertContains(response,'Alex Ssanya')
+
