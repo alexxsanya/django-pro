e0ca01f7317534055569a72f357622fd75068f68
Alex Ssanya <alexssanya@gmail.com>
2019-05-26 17:12:00 +0300

    add user model 165967586

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class User:
+    users_db = []
+    def __init__(self, fullname=None,email=None,password=None):
+        self.fullname = fullname
+        self.email = email
+        self.password = password
+
+    def save_user(self): 
+        self.users_db.append({
+            'fullname':self.fullname,
+            'email':self.email,
+            'password':self.password
+        })
+
+    @staticmethod
+    def get_all_users():
+        return User.users_db
+
+    
