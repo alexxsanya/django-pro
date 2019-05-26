6e7e392fc62ac34e1f9aa8074ae54c4070e66300
Alex Ssanya <alexssanya@gmail.com>
2019-05-26 17:13:16 +0300

    add signup view #165967586

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from users.models import User
 
-# Create your views here.
+def signup_users(request): 
+
+    data = {
+        'fullname':'Alex Ssanya',
+        'email': 'alex@gmail.com',
+        'password': 'Password'
+    }
+
+    User(**data).save_user()
+    
+    get_all_users = User.get_all_users()
+    
+    return HttpResponse(json.dumps(get_all_users))
